ee4165b6a305c0f4f00914c202cac961a339c45f
modzxspirit <86035405+modzxspirit@users.noreply.github.com>
2022-05-17 01:27:51 +0100

    Update bru.lua

--- bru.lua
+++ bru.lua
@@ -76,7 +76,7 @@ local Initialize = function(Side)
             v.ChildAdded:Connect(function(_)
                 repeat task.wait() until _.AbsolutePosition.Y >= math.floor(Y)
                 if library.flags.AP and Keys[_.Parent.Name]~=nil then
-                    game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
+                    game:GetService'VirtualInputManager':SendKeyEvent(true,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
                     if #Arrows.LongNotes[_.Parent.Name]:children()==0 then 
                         game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
  
@@ -87,7 +87,7 @@ local Initialize = function(Side)
             v.ChildAdded:Connect(function(_)
                 repeat task.wait() until _.AbsolutePosition.Y <= math.ceil(Y)
                 if library.flags.AP and Keys[_.Parent.Name]~=nil then
-                    game:GetService'VirtualInputManager':SendKeyEvent(true,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
+                    game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
                     if #Arrows.LongNotes[_.Parent.Name]:children()==0 then 
                         game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
                         game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
@@ -100,14 +100,14 @@ local Initialize = function(Side)
         if ScrollType(Side)=="Downscroll"then
             v.ChildAdded:Connect(function(ln)
                 repeat task.wait() until ln.AbsolutePosition.Y-75 >= Y
-                game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[ln.Parent.Name]],false,nil)
+                game:GetService'VirtualInputManager':SendKeyEvent(true,Enum.KeyCode[Keys[ln.Parent.Name]],false,nil)
                 
                 ln:Destroy() 
             end)
         else
             v.ChildAdded:Connect(function(ln)
                 repeat task.wait() until ln.AbsolutePosition.Y+75 <= Y
-                game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[ln.Parent.Name]],false,nil)
+                game:GetService'VirtualInputManager':SendKeyEvent(true,Enum.KeyCode[Keys[ln.Parent.Name]],false,nil)
                 
                 ln:Destroy() 
             end)
