c085c2ea1f21919df0136303105e3f3c017c5b42
modzxspirit <86035405+modzxspirit@users.noreply.github.com>
2022-05-17 01:33:39 +0100

    Update bru.lua

--- bru.lua
+++ bru.lua
@@ -76,7 +76,7 @@ local Initialize = function(Side)
             v.ChildAdded:Connect(function(_)
                 repeat task.wait() until _.AbsolutePosition.Y >= math.floor(Y)
                 if library.flags.AP and Keys[_.Parent.Name]~=nil then
-                    game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
+                    game:GetService'VirtualInputManager':SendKeyEvent(true,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
                     if #Arrows.LongNotes[_.Parent.Name]:children()==0 then 
                         game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
  
@@ -89,7 +89,6 @@ local Initialize = function(Side)
                 if library.flags.AP and Keys[_.Parent.Name]~=nil then
                     game:GetService'VirtualInputManager':SendKeyEvent(true,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
                     if #Arrows.LongNotes[_.Parent.Name]:children()==0 then 
-                        game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
                         game:GetService'VirtualInputManager':SendKeyEvent(false,Enum.KeyCode[Keys[_.Parent.Name]],false,nil)
                     end
                 end
